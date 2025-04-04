4e1de17e03e320d12392720e50391a8ddb09d9d1
antdp <phucanclone97@gmail.com>
2025-04-04 23:15:48 +0700

    Add @vercel/speed-insights dependency and integrate SpeedInsights component into layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Link from "next/link";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -67,6 +68,7 @@ export default function RootLayout({
           </header>
 
           {children}
+          <SpeedInsights />
         </div>
       </body>
     </html>
